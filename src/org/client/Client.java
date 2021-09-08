f74ea40f8678206e6e16896c001b3462ae699998
AJAI04 <dl.ajaihalins@gmail.com>
2021-09-08 16:02:15 +0530

    ajai

---
+++ src/org/client/Client.java
@@ -0,0 +1,12 @@
+package org.client;
+
+public class Client {
+	public void clientName() {System.out.println("Client Name is Jack");
+		// TODO Auto-generated method stub
+
+	}
+public static void main(String[] args) {
+	Client c= new Client();
+	c.clientName();
+}
+}
---
+++ src/org/company/company.java
@@ -0,0 +1,14 @@
+package org.company;
+import org.client.*;
+public class company extends Client {private void CompanyName() {System.out.println("Company Name is Twitter");
+
+	// TODO Auto-generated method stub
+
+}
+public static void main(String[] args) {
+	company c= new company();
+	c.CompanyName();
+	c.clientName();
+}
+
+}
